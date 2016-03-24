5d356318635a790ea4be0499cf12f16acb145d4e
lesterarte <lesterarte007@unitec.edu>
2016-03-24 03:59:45 -0600

    Terminado ambiente grafico

--- main.cpp
+++ main.cpp
@@ -4,50 +4,215 @@
 #include <stdio.h> 
 
 void mapa(); 
+void mapapiezas(char**, int,int);
 void crearpremios(char**,int, int);
 void deletepremios(char**,int, int);
 void signedpremios(char**,int,int);
+void rules();
+bool existencia(char**,char,char);
+void clearmap(char**, int,int);
+
 int main(int argc, char*argv[])
 {
 	int filas = 3; 
 	int columnas= 4; 
 	char apuesta = ' '; 
-	int cont = 1; 
+	int cont = 0;
+	int contar=0;
+	int turnos=0; 
+	char pocision[2];
 	char** arraypremios = new char*[filas];
 	srand(time(NULL));
+	crearpremios(arraypremios,filas,columnas);
+	signedpremios(arraypremios,filas,columnas);
 	initscr();
 	refresh();
 	echo();
 	mvprintw(9,20,"***************************************** BIENVENIDO **************************************");
 	mvprintw(10,17,"****************************************** TIRO AL BLANCO ***************************************"); 
-	mvprintw(15,20, "INGRESE LA CANTIDAD DE DINERO QUE DESEA APOSTAR: "); 
+	mvprintw(15,20, "1. JUGAR  "); 
+	mvprintw(16,20, "2. COMO JUGAR??");
+	mvprintw(17,20, "3. SALIR");
+	move(18,20);
 
-	while( apuesta != '\n')
+	while( cont != 1 )
 	{
 		noecho();
 		apuesta = getch();
-		if( cont > 1 && apuesta >= 48  && apuesta <58)
+		if(apuesta >= 49 && apuesta <= 51)
 		{
 			addch(apuesta);
 			echo();
+			cont++;
 		}
-		else if(apuesta >= 49  && apuesta <58)
-		{
-			addch(apuesta);
-			echo();
-			cont++; 
-		}
-		
 	}
-
+	cont=0; 
 	mvprintw(9,20,"                                                                                                 ");
 	mvprintw(10,17,"                                                                                                        "); 
-	mvprintw(15,20, "                                                                "); 
-	mapa();
-	getch();
+	mvprintw(15,20, "                 ");
+	mvprintw(16,20, "                    ");
+	mvprintw(17,20, "          ");
+
+	while(apuesta != 51)
+	{
+		if(apuesta == 49)
+		{
+			signedpremios(arraypremios,filas,columnas);
+			clear();
+			refresh();
+			mapa();
+			mapapiezas(arraypremios,filas,columnas);
+			refresh();
+			move(11,117);
+			char apuntar;
+						 
+			while(turnos < 3)
+			{
+				while( contar  != 2)
+				{
+					noecho();
+					apuntar = getch();
+					if(apuntar >= 48 && apuntar <= 57)
+					{
+						if(contar > 0)
+						{
+							if(pocision[0] == 48)
+							{
+								if(apuntar > 48 && apuntar <= 57)
+								{
+									pocision[1] = apuntar; 
+									addch(apuntar);
+									echo();
+									contar++;
+								}
+							}
+							else
+							{
+								if(apuntar == 48 || apuntar == 49 || apuntar == 50)
+								{
+									pocision[1] = apuntar; 
+									addch(apuntar);
+									echo();
+									contar++;
+								}
+							}
+						}
+						else
+						{
+							if(apuntar == 48 || apuntar == 49 )
+							{
+								pocision[0] = apuntar; 
+								addch(apuntar);
+								echo();
+								contar++;
+							}
+						}
+					}
+				}//fin de while de ingreso de coordenada	
+				contar=0;			
+				move(11,117);
+				if(existencia(arraypremios,pocision[0],pocision[1]) == true)
+				{
+					clearmap(arraypremios,filas,columnas);
+					mapapiezas(arraypremios,filas,columnas);
+					refresh();
+					mvprintw(18,90,"FELICIDADES ACERTASTE ");
+					getch();
+					
+
+				}
+				else
+				{
+					clearmap(arraypremios,filas,columnas);
+					mapapiezas(arraypremios,filas,columnas);
+					refresh();
+					mvprintw(18,90,"PERDISTE, NO HAY PREMIO ");
+					getch();
+				}
+
+				mvprintw(11,117,"   ");
+				mvprintw(18,90,"                             ");
+				move(11,117);
+				apuntar=' ';
+
+				turnos++;
+			}
+		}
+
+		if(apuesta == 50)
+		{
+			clear();
+			refresh();
+			rules();
+			getch();
+		}
+		clear();
+		refresh();
+		mvprintw(9,20,"***************************************** BIENVENIDO **************************************");
+		mvprintw(10,17,"****************************************** TIRO AL BLANCO ***************************************"); 
+		mvprintw(15,20, "1. JUGAR  "); 
+		mvprintw(16,20, "2. COMO JUGAR??");
+		mvprintw(17,20, "3. SALIR");
+		move(18,20);
+
+		while( cont != 1 )
+		{
+			noecho();
+			apuesta = getch();
+			if(apuesta >= 49 && apuesta <= 51)
+			{
+				addch(apuesta);
+				echo();
+				cont++;
+			}
+		}
+		cont=0; 
+		mvprintw(9,20,"                                                                                                 ");
+		mvprintw(10,17,"                                                                                                        "); 
+		mvprintw(15,20, "                 ");
+		mvprintw(16,20, "                    ");
+		mvprintw(17,20, "   ");
+	}
+
 	endwin();
+	deletepremios(arraypremios,filas,columnas);
 	return 0; 
+}//fin  main 
+
+void clearmap(char** disparo,int fil,int cols)
+{
+	for(int i=0; i<fil;i++)
+	{
+		for(int j=0; j<cols;j++)
+		{
+			mvprintw((5 + (i*12)),(17 + (j*17)), "    ");
+			mvprintw((6 + (i*12)),(17 + (j*17)), "    ");
+			mvprintw((7 + (i*12)),(12 + (j*17)), "          ");
+			mvprintw((8 + (i*12)),(12 + (j*17)), "          ");
+			mvprintw((9 + (i*12)),(17 + (j*17)), "    ");
+			mvprintw((10 + (i*12)),(17 + (j*17)), "     ");
+			mvprintw((11 + (i*12)),(16 + (j*17)), "       ");
+			mvprintw((12 + (i*12)),(15 + (j*17)), "          ");
+		}
+	}	
 }
+void rules()
+{
+	mvprintw(1,35,"******************** INSTRUCCIONES DE JUEGO **********************");
+	mvprintw(5,10,"1. APOSTAR: ");
+	mvprintw(7,10,"Al comenzar el juego cada jugador invierte una suma de dinero la cual es su capital para jugar.");
+	mvprintw(8,10,"El juego termina cuando el jugador termina sus 3 tiros");
+	mvprintw(9,10,"cada set de 3 tiros tiene un valor de $ 100");
+	mvprintw(11,10,"2. TIROS: ");
+	mvprintw(13,10,"El jugador cuenta con 3 tiros los cuales realizara de forma consecutiva y los que se premiaran de la siguiente manera:");
+	mvprintw(14,13,"2.1 - si el jugador acierta 1 tiro de 3 su ganancia sera el 0.10 de su apuesta");
+	mvprintw(15,13,"2.2 - si el jugador acierta 2 tiros de 3 su ganancia sera el 0.35 de su apuesta");
+	mvprintw(16,13,"2.3 - si el jugador acierta 3 tiros de 3 su ganancia sera el 0.50 de su apuesta");
+	mvprintw(18,10,"Si el jugador no aciera ningun juego se le preguntara si desea seguir jugando");
+	mvprintw(30,10, "Cualquier tecla para continuar ");
+
+}
+
 
 void crearpremios(char** premios,int fil, int cols)
 {
@@ -83,48 +248,130 @@ void signedpremios(char** premios, int fil, int cols)
 	}	
 }
 
+bool existencia(char** premios,char first, char second)
+{
+
+	int xchar= first - '0';
+	int ychar= second - '0';
+	int x,y; 
+	if(xchar == 0)
+	{
+		if(ychar == 1)
+		{
+			x=0;
+			y=0;
+		}
+
+		if(ychar == 2)
+		{
+			x=0;
+			y=1;
+		}
+		if(ychar == 3)
+		{
+			x=0;
+			y=2;
+		}
+		if(ychar == 4)
+		{
+			x=0;
+			y=3;
+		}
+		if(ychar == 5)
+		{
+			x=1;
+			y=0;
+		}
+		if(ychar == 6)
+		{
+			x=1;
+			y=1;
+		}
+		if(ychar == 7)
+		{
+			x=1;
+			y=2;
+		}
+		if(ychar == 8)
+		{
+			x=1;
+			y=3;
+		}
+		if(ychar == 9)
+		{
+			x=2;
+			y=0;
+		}
+	}
+	else
+	{
+		if(ychar == 0)
+		{
+			x=2;
+			y=1;
+		}
+
+		if(ychar == 1)
+		{
+			x=2;
+			y=2;
+		}
+		if(ychar == 2)
+		{
+			x=2;
+			y=3;
+		}
+	}
+
+	if(premios[x][y] == '#')
+	{
+		premios[x][y] = '-';
+		return true; 
+	}
+	else
+	{
+		premios[x][y] = '-';
+		return false; 
+	}
+}
+
 void mapa()
 {
 	
 	mvprintw(1,20,"***************************************** BIENVENIDO **************************************");
 	mvprintw(2,17,"****************************************** TIRO AL BLANCO ***************************************");
 
-	mvprintw(5,8,"         ****             ****             ****             ****   ");
-	mvprintw(6,8,"         ****             ****             ****             ****   ");
-	mvprintw(7,8,"    *********        *********        *********        *********  ");
-	mvprintw(8,8,"    *********        *********        *********        *********   ");
-	mvprintw(9,8,"         ****             ****             ****             ****   ");
-	mvprintw(10,8,"        *****            *****            *****            *****   ");
-	mvprintw(11,8,"       *** ***          *** ***          *** ***          *** ***   ");
-	mvprintw(12,8,"      ***   ***	      ***    ***        ***   ***        ***   ***   ");
-	mvprintw(13,8,"     +---------+      +---------+      +---------+      +--------+   "); 
-	mvprintw(14,8,"     |    1    |      |     2   |      |    3    |      |   4    |   ");  
-	mvprintw(15,8,"-----+---------+------+---------+------+---------+------+--------+-----  "); 
-	mvprintw(17,8,"         ****             ****             ****             ****   ");
-	mvprintw(18,8,"         ****             ****             ****             ****   ");
-	mvprintw(19,8,"    *********        *********        *********        *********  ");
-	mvprintw(20,8,"    *********        *********        *********        *********   ");
-	mvprintw(21,8,"         ****             ****             ****             ****   ");
-	mvprintw(22,8,"        *****            *****            *****            *****   ");
-	mvprintw(23,8,"       *** ***          *** ***          *** ***          *** ***   ");
-	mvprintw(24,8,"      ***   ***	      ***    ***        ***   ***        ***   ***   ");
-	mvprintw(25,8,"     +---------+      +---------+      +---------+      +--------+   "); 
-	mvprintw(26,8,"     |    5    |      |     6   |      |    7    |      |   8    |   ");  
-	mvprintw(27,8,"-----+---------+------+---------+------+---------+------+--------+-----  "); 
-	mvprintw(28,8,"         ****             ****             ****             ****   ");
-	mvprintw(29,8,"         ****             ****             ****             ****   ");
-	mvprintw(30,8,"    *********        *********        *********        *********  ");
-	mvprintw(31,8,"    *********        *********        *********        *********   ");
-	mvprintw(32,8,"         ****             ****             ****             ****   ");
-	mvprintw(33,8,"        *****            *****            *****            *****   ");
-	mvprintw(34,8,"       *** ***          *** ***          *** ***          *** ***   ");
-	mvprintw(35,8,"      ***   ***	      ***    ***        ***   ***        ***   ***   ");
-	mvprintw(36,8,"     +---------+      +---------+      +---------+      +--------+   "); 
-	mvprintw(37,8,"     |    9    |      |    10   |      |    11   |      |   12   |   ");  
-	mvprintw(38,8,"-----+---------+------+---------+------+---------+------+--------+-----  "); 
-
-	mvprintw(19,90,"INGRESE LA POCISION DE LA PIEZA");
-	mvprintw(20,90,"A LA QUE DESEA DISPARAR:  ");
- 
+	
+	mvprintw(13,9,"     +---------+      +---------+      +---------+       +--------+   "); 
+	mvprintw(14,9,"     |    01   |      |    02   |      |   03    |       |   04   |   ");  
+	mvprintw(15,9,"-----+---------+------+---------+------+---------+-------+--------+-----  "); 
+	mvprintw(25,9,"     +---------+      +---------+      +---------+       +--------+   "); 
+	mvprintw(26,9,"     |    05   |      |    06   |      |    07   |       |   08   |   ");  
+	mvprintw(27,9,"-----+---------+------+---------+------+---------+-------+--------+-----  "); 
+	mvprintw(37,9,"     +---------+      +---------+      +---------+       +--------+   "); 
+	mvprintw(38,9,"     |    09   |      |   10    |      |   11    |       |   12   |   ");  
+	mvprintw(39,9,"-----+---------+------+---------+------+---------+-------+--------+-----  "); 
+	mvprintw(10,90,"INGRESE LA POCISION DE LA PIEZA");
+	mvprintw(11,90,"A LA QUE DESEA DISPARAR:  ");
 }
 
+void mapapiezas(char** disparo,int fil,int cols)
+{
+	for(int i=0; i<fil;i++)
+	{
+		for(int j=0; j<cols;j++)
+		{
+			if(disparo[i][j] != '-')
+			{
+				mvprintw((5 + (i*12)),(17 + (j*17)), "****");
+				mvprintw((6 + (i*12)),(17 + (j*17)), "****");
+				mvprintw((7 + (i*12)),(12 + (j*17)), "*********");
+				mvprintw((8 + (i*12)),(12 + (j*17)), "*********");
+				mvprintw((9 + (i*12)),(17 + (j*17)), "****");
+				mvprintw((10 + (i*12)),(17 + (j*17)), "*****");
+				mvprintw((11 + (i*12)),(16 + (j*17)), "*** ***");
+				mvprintw((12 + (i*12)),(15 + (j*17)), "***   ***");
+			}
+		}
+	}	
+}
